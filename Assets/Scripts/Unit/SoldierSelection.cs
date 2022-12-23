272759e6bfbe456d4165a36ceac5debc9c42eb19
David <d.weckerly@gmail.com>
2022-12-23 17:29:03 -0600

    stubbing for soldier selection

---
+++ Assets/Scripts/Unit/SoldierSelection.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoldierSelection : MonoBehaviour
+{
+    const float SELECTED_SOLDIER_HEIGHT = 0.3f;
+    public GameObject[] Soldiers;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
